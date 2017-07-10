21bd8d227a84eb21b35faf5e47014fdd10db4444
Christopher Brown <io@henrian.com>
2017-07-10 11:40:57 -0500

    Add more explicit deps and dev tools to project config

--- project.clj
+++ project.clj
@@ -5,11 +5,17 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [chbrown/data.json "0.2.7"]
-                 [twttr "2.0.0"]]
+                 [twttr "2.0.0"]
+                 [manifold "0.1.6"]
+                 [byte-streams "0.2.3" :exclusions [manifold]]
+                 [byte-transforms "0.1.4" ]]
   :exclusions [org.clojure/data.json]
   :main twttr.cli
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]]
+             :dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
+                                  [org.clojure/tools.trace "0.7.9"]]
                    :source-paths ["dev" "src"]
                    :repl-options {:init-ns user}}})
