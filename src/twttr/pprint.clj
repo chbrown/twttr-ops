97f8ac94db6e9d75d5988a2418c162e249d014ba
Christopher Brown <io@henrian.com>
2017-07-21 11:30:02 -0500

    Add twttr.pprint module with some parsing and formatting helpers

---
+++ src/twttr/pprint.clj
@@ -0,0 +1,66 @@
+(ns twttr.pprint
+  (:require [clojure.string :as str]))
+
+(defn normalize-whitespace
+  "Replace each whitespace token with a normal space"
+  [string]
+  (some-> string (str/replace #"\s" " ")))
+
+(def ^:private twitter-datetime-formatter
+  "Twitter uses an unusual DateTime format; matched by this custom formatter"
+  (java.time.format.DateTimeFormatter/ofPattern "EEE MMM dd HH:mm:ss Z yyyy"))
+
+(defn parse-datetime
+  "Parses Twitter's unusual datetime format (e.g., the value of the `created_at` field)
+  as a standard Java8 OffsetDateTime instance."
+  {:test (fn []
+           (assert (= "2010-04-01T01:23:45Z" (str (parse-datetime "Mon Apr 01 01:23:45 +0000 2010"))))
+           ; #inst "2008-04-14T02:13:55Z"
+           (assert (= "2008-04-14T02:13:55Z" (str (parse-datetime "Mon Apr 14 02:13:55 +0000 2008"))))
+           (assert (= "2008-08-27T13:08:45Z" (str (parse-datetime "Wed Aug 27 13:08:45 +0000 2008")))))}
+  [text]
+  (java.time.OffsetDateTime/parse text twitter-datetime-formatter))
+
+(defn normalize-status
+  [status]
+  (-> status
+      (update :created_at parse-datetime)
+      (update :text normalize-whitespace)
+      (update-in [:user :name] normalize-whitespace)
+      (update-in [:user :screen_name] normalize-whitespace)
+      (dissoc :contributors :geo) ; these are deprecated
+      ; Clojure data.json has Longs covered:
+      (dissoc :id_str :in_reply_to_status_id_str :in_reply_to_user_id_str :quoted_status_id_str)
+      (update :user dissoc :id_str)))
+
+;; reply-to tree formatting
+
+(defn- space [n] (str/join (repeat n " ")))
+
+(defn- format-status ; short
+  [status]
+  (normalize-whitespace
+   (str (:id status) " " "@" (:screen_name (:user status)) ": " (:text status))))
+
+(defn- format-reply-branch
+  "Iterate over statuses, returning lines in a tree format.
+  replies is a mapping from original status ID to a list of replies to that status ID"
+  ([status replies]
+   (format-reply-branch status replies 0))
+  ([status replies level]
+   (cons (str (space (* level 2)) (format-status status))
+         (mapcat #(format-reply-branch % replies (inc level)) (get replies (:id status))))))
+
+(defn format-reply-tree
+  "Returns list of lines"
+  [initial-statuses]
+  (let [statuses (dedupe (sort-by (comp - :id)) initial-statuses)
+        ; Create a mapping from status IDs to replies (to that status)
+        replies (reduce (fn [m status]
+                          ; add the status to the list at m[in_reply_to_status_id]
+                          (update m (:in_reply_to_status_id status) conj status)) {} statuses)
+        ; Every status has at most one :in_reply_to_status_id value;
+        ; if it has one, that is its parent; if it doesn't, it is a root node.
+        ; Start with the root nodes, which are those that are in reply to nothing.
+        roots (get replies nil)]
+    (mapcat #(format-reply-branch % replies) roots)))
