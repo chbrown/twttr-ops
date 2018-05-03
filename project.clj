a4247e6d05b353a34e9a8d361b977af4d8c818a3
Christopher Brown <io@henrian.com>
2018-05-02 19:46:52 -0500

    Update tools.cli version

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                               [:name "Christopher Brown"]
                               [:email "io@henrian.com"]]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/tools.cli "0.3.7"]
                  [org.clojure/tools.logging "0.4.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [chbrown/data.json "0.2.7"]
